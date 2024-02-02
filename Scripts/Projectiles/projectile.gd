b88ad544d8e29273be50700c9ab132abfe69170a
nealholt <neal.holts@gmail.com>
2024-02-01 20:51:45 -0700

    Shield improvements and tests
    I tested out that the projectile.gd ignore list actually works and was surprised to learn that it doesn't, because it only triggers on area entered. So I don't need it at all. I can just ignore areas that are entered immediately! I added a check to verify that those areas are attached to shields using a group. I also verified that enemies could shoot the player from within the player's shield without first taking down the shield.

--- Scripts/Projectiles/projectile.gd
+++ Scripts/Projectiles/projectile.gd
@@ -16,10 +16,6 @@ var target # Used for projectiles that seek
 var velocity : Vector3
 var acceleration := Vector3.ZERO #non zero probably only for missiles
 
-# This is a list of bodies and areas to ignore.
-# Currently it is used for shields.
-var ignore_list : Array = Array()
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print("bullet created")
@@ -98,23 +94,16 @@ func damage_and_die(body):
 	# In order to fire from within a shield, we need
 	# to ignore immediate collisions. So if any
 	# bullet collides with another object in the first
-	# 2/100ths of a second, ignore that collision and
-	# add the collided object to an ignore list.
+	# 2/100ths of a second AND that body is a shield,
+	# then ignore that collision.
 	# 2/100ths was found through testing to be a good
 	# cut off.
-	if $Timer.wait_time - $Timer.time_left <= 0.02:
-		ignore_list.push_back(body)
+	if $Timer.wait_time - $Timer.time_left <= 0.02 and body.get_groups().has("shield"):
 		return
 	
 	# Testing
 	#print($Timer.wait_time - $Timer.time_left)
 	
-	# Check for bodies that should be ignored.
-	# This is for firing from within a shield.
-	# Bullets should pass out of shields, but not
-	# enter the shield.
-	if ignore_list.has(body):
-		return
 	#https://www.youtube.com/watch?v=LuUjqHU-wBw
 	if body.is_in_group("damageable"):
 		#print("dealt damage")
