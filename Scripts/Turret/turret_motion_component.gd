a37a0859e294d69b12c863b89c369bda05a3faea
nealholt <neal.holts@gmail.com>
2024-08-31 20:08:19 -0600

    Consolidated turret code
    turret_motion_component can be attached to anything that wants to rotate like a turret and then passed the body and head to rotate.

---
+++ Scripts/Turret/turret_motion_component.gd
@@ -0,0 +1,94 @@
+extends Node
+
+# Pass the body to be rotated to the function below
+# ala this tutorial at about 5:30:
+# https://www.youtube.com/watch?v=EMpXt2MLx_4
+
+# movement speeds and constraints in degrees
+@export var elevation_speed_deg: float = 5
+@export var rotation_speed_deg: float = 5
+@export var min_elevation_deg: float = 0
+@export var max_elevation_deg: float = 60
+
+# movement speeds and constraints in radians
+@onready var elevation_speed: float = deg_to_rad(elevation_speed_deg)
+@onready var rotation_speed: float = deg_to_rad(rotation_speed_deg)
+@onready var min_elevation: float = deg_to_rad(min_elevation_deg)
+@onready var max_elevation: float = deg_to_rad(max_elevation_deg)
+
+# The following is loosely based on code from here:
+# https://github.com/IndieQuest/ModularTurret/tree/master/src
+# https://www.youtube.com/watch?v=4IS9zIVCDKc&ab_channel=IndieQuest
+# but modified by Neal Holtschulte in 2024 because
+# 1. That code didn't work for me and 
+# 2. it was designed to only work when the turret
+# was parallel to the ground.
+# This was a life saver:
+# https://math.stackexchange.com/questions/728481/3d-projection-onto-a-plane
+func rotate_and_elevate(body:Node3D, head:Node3D, delta:float, current_target:Vector3) -> void:
+	# Project the target onto the XZ plane of the turret
+	# but first adjust by the global position because
+	# the global basis is purely orientation, not position.
+	var rotation_targ:Vector3 = get_projected(current_target - body.global_position, body.global_basis.y)
+	# But! You also need to account for changes in position,
+	# so add back in the global position adjustment.
+	rotation_targ = rotation_targ + body.global_position
+
+	# Get the angle from the body's facing direction (z)
+	# to the projected point. Since the point is projected
+	# onto the plane, this should be the angle the body
+	# should rotate to face along only one axis.
+	var y_angle:float = Global.get_angle_to_target(body.global_position, rotation_targ, body.global_basis.z)
+	
+	# Rotate toward target
+	# Calculate sign to rotate left or right.
+	var rotation_sign:float = sign(body.to_local(current_target).x)
+	# Calculate step size and direction. Use min to avoid
+	# over-rotating. Just snap to the desired angle if it's
+	# less than what we would rotate this frame.
+	var final_y:float = rotation_sign * min(rotation_speed * delta, y_angle)
+	body.rotate_y(final_y)
+	
+	# Rotation is complete, now we elevate.
+	# Project the target onto the ZY plane of the head
+	# but first adjust by the global position because
+	# the global basis is purely orientation, not position.
+	var elevation_targ:Vector3 = get_projected(current_target - head.global_position, head.global_basis.x)
+	# But! You also need to account for changes in position,
+	# so add back in the global position adjustment.
+	elevation_targ = elevation_targ + head.global_position
+	
+	# Get the angle from the head's facing direction (z)
+	# to the projected point. Since the point is projected
+	# onto the plane, this should be the angle the head
+	# should rotate to face along only one axis.
+	var x_angle:float = Global.get_angle_to_target(head.global_position, elevation_targ, head.global_basis.z)
+	
+	# Elevate toward target.
+	# Calculate sign to elevate up or down.
+	# There's an extra negative sign because pitching up is negative.
+	var elevation_sign:float = -sign(head.to_local(current_target).y)
+	# Calculate step size and direction. Use min to avoid
+	# over-rotating. Just snap to the desired angle if it's
+	# less than what we would rotate this frame.
+	var final_x:float = elevation_sign * min(elevation_speed * delta, x_angle)
+	head.rotate_x(final_x)
+	# Clamp elevation within limits.
+	# Reverse and negate max and min because up is negative and
+	# down is positive.
+	head.rotation.x = clamp(
+		head.rotation.x,
+		-max_elevation, min_elevation
+	)
+
+
+func get_projected(pos:Vector3, normal:Vector3) -> Vector3:
+	# Project position "pos" onto the plane with the given normal vector.
+	# https://math.stackexchange.com/questions/728481/3d-projection-onto-a-plane
+	# "projected" is the vector indicating how far above/below
+	# the target is from the plane of rotation.
+	normal = normal.normalized()
+	var projection:Vector3 = (pos.dot(normal) / normal.dot(normal)) * normal
+	# By subtracting projection from position, we get the
+	# projected point.
+	return pos - projection
--- Scripts/camera_player_group.gd
+++ Scripts/camera_player_group.gd
@@ -19,7 +19,6 @@ var target:Node3D
 const target_close_up_dist:=-30.0
 
 var look_at_target : bool = false
-@export var look_speed : float = 59.0
 
 func _physics_process(delta: float) -> void:
 	if state == CameraState.FLYBY:
@@ -36,108 +35,13 @@ func _physics_process(delta: float) -> void:
 			first_person()
 	# Look at target with first-person cam
 	if look_at_target and state == CameraState.FIRSTPERSON and is_instance_valid(target):
-		rotate_and_elevate(delta, $Body, $Body/Head, target.global_position)
-		#first_person_camera.look_at(target.global_position, Global.player.global_transform.basis.y)
-		
-		# Here's where I tried it using the code copied from 
-		# Global.interp_face_target. Doesn't work.
-		#var new_transform = first_person_camera.transform.looking_at(target.global_position,first_person_camera.global_transform.basis.y)
-		#first_person_camera.transform = first_person_camera.transform.interpolate_with(new_transform, delta*look_speed)
-		
-		#first_person_camera.rotation = lerp(first_person_camera.rotation, first_person_camera.get_angle_to(target.global_position),look_speed)
-		#first_person_camera.rotation = lerp_angle(from, to, weight)
-		#first_person_camera.rotation.y=lerp_angle(first_person_camera.rotation.y,atan2(target.global_position.x,target.global_position.z),look_speed*delta)
-		#first_person_camera.rotation.z=lerp(first_person_camera.rotation.z,atan2(-target.global_position.x,-target.global_position.y),look_speed*delta)
-		#first_person_camera.rotation.x=lerp(first_person_camera.rotation.x,atan2(-target.global_position.y,-target.global_position.z),look_speed*delta)
-		#print(first_person_camera.global_position.direction_to(target.global_position))
-		#first_person_camera.global_transform.basis.z = lerp(first_person_camera.global_transform.basis.z, -first_person_camera.global_position.direction_to(target.global_position), look_speed*delta)
-		#first_person_camera.global_transform.basis.z = lerp(first_person_camera.global_transform.basis.z, -first_person_camera.get_angle_to(target.global_position), look_speed*delta)
-		#first_person_camera.transform = Global.interp_face_target(first_person_camera, target.global_position, look_speed*delta)
-		
+		$turret_motion_component.rotate_and_elevate($Body, $Body/Head, delta, target.global_position)
 	else:
 		# Return to facing forward, or at least way far
 		# forward of the nose of the player.
 		# TODO fix this next bit and just have a Node3D attached to the player straight ahead
 		var temp_targ_pos : Vector3 = first_person_camera.global_position - Global.player.global_transform.basis.z*10000.0
-		rotate_and_elevate(delta, $Body, $Body/Head, temp_targ_pos)
-		#first_person_camera.look_at(first_person_camera.global_position - Global.player.global_transform.basis.z*10000.0, Global.player.global_transform.basis.y)
-		
-		# Here's where I tried it using the code copied from 
-		# Global.interp_face_target but with the player's up
-		# as the "look"s up. Doesn't work.
-		#var new_transform = first_person_camera.transform.looking_at(first_person_camera.global_position - Global.player.global_transform.basis.z*10000.0, Global.player.global_transform.basis.y)
-		#first_person_camera.transform = first_person_camera.transform.interpolate_with(new_transform, delta*look_speed)
-		
-		#first_person_camera.transform = Global.interp_face_target(first_person_camera, first_person_camera.global_position - Global.player.global_transform.basis.z*10000.0, look_speed*delta)
-		#var new_transform = first_person_camera.transform.looking_at(first_person_camera.global_position - Global.player.global_transform.basis.z*10000.0, Global.player.global_transform.basis.y)
-		#first_person_camera.transform = first_person_camera.transform.interpolate_with(new_transform, look_speed*delta)
-
-
-
-# The following is duplicated in turret.gd
-# I was trying to smooth out target look, but
-# this didn't work.
-func rotate_and_elevate(delta: float, body:Node3D, head:Node3D, current_target:Vector3) -> void:
-	var rotation_speed : float = 3.0
-	var elevation_speed : float = 3.0
-	var min_elevation: float = deg_to_rad(0.0)
-	var max_elevation: float = deg_to_rad(70.0)
-	# Project the target onto the XZ plane of the turret
-	# but first adjust by the global position because
-	# the global basis is purely orientation, not position.
-	var rotation_targ:Vector3 = get_projected(current_target - body.global_position, body.global_basis.y)
-	# But! You also need to account for changes in position,
-	# so add back in the global position adjustment.
-	rotation_targ = rotation_targ + body.global_position
-
-	# Get the angle from the body's facing direction (z)
-	# to the projected point. Since the point is projected
-	# onto the plane, this should be the angle the body
-	# should rotate to face along only one axis.
-	var y_angle:float = Global.get_angle_to_target(body.global_position, rotation_targ, body.global_basis.z)
-	
-	# Rotate toward target
-	# Calculate sign to rotate left or right.
-	var rotation_sign:float = sign(body.to_local(current_target).x)
-	# Calculate step size and direction. Use min to avoid
-	# over-rotating. Just snap to the desired angle if it's
-	# less than what we would rotate this frame.
-	var final_y:float = rotation_sign * min(rotation_speed * delta, y_angle)
-	body.rotate_y(final_y)
-	
-	# Rotation is complete, now we elevate.
-	# Project the target onto the ZY plane of the head
-	# but first adjust by the global position because
-	# the global basis is purely orientation, not position.
-	var elevation_targ:Vector3 = get_projected(current_target - head.global_position, head.global_basis.x)
-	# But! You also need to account for changes in position,
-	# so add back in the global position adjustment.
-	elevation_targ = elevation_targ + head.global_position
-	
-	# Get the angle from the head's facing direction (z)
-	# to the projected point. Since the point is projected
-	# onto the plane, this should be the angle the head
-	# should rotate to face along only one axis.
-	var x_angle:float = Global.get_angle_to_target(head.global_position, elevation_targ, head.global_basis.z)
-	
-	# Elevate toward target.
-	# Calculate sign to elevate up or down.
-	# There's an extra negative sign because pitching up is negative.
-	var elevation_sign:float = -sign(head.to_local(current_target).y)
-	# Calculate step size and direction. Use min to avoid
-	# over-rotating. Just snap to the desired angle if it's
-	# less than what we would rotate this frame.
-	var final_x:float = elevation_sign * min(elevation_speed * delta, x_angle)
-	head.rotate_x(final_x)
-	# Clamp elevation within limits.
-	# Reverse and negate max and min because up is negative and
-	# down is positive.
-	head.rotation.x = clamp(
-		head.rotation.x,
-		-max_elevation, min_elevation
-	)
-
-
+		$turret_motion_component.rotate_and_elevate($Body, $Body/Head, delta, temp_targ_pos)
 
 
 # Turn on looking at player's target
@@ -217,17 +121,3 @@ func view_target_from_player() -> void:
 	free_camera.global_position = global_position + \
 		free_camera.transform.basis.z*10 + \
 		Vector3.UP*5
-
-
-
-# TODO TESTING
-func get_projected(pos:Vector3, normal:Vector3) -> Vector3:
-	# Project position "pos" onto the plane with the given normal vector.
-	# https://math.stackexchange.com/questions/728481/3d-projection-onto-a-plane
-	# "projected" is the vector indicating how far above/below
-	# the target is from the plane of rotation.
-	normal = normal.normalized()
-	var projection:Vector3 = (pos.dot(normal) / normal.dot(normal)) * normal
-	# By subtracting projection from position, we get the
-	# projected point.
-	return pos - projection
