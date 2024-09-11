5e9c772ec86efa62e9573427cdd81dc8661b06a6
nealholt <neal.holts@gmail.com>
2024-09-11 14:34:45 -0600

    Simplified weapon handler

--- Scripts/weapon_handler.gd
+++ Scripts/weapon_handler.gd
@@ -3,18 +3,12 @@ extends Node3D
 # Source
 # https://www.udemy.com/course/complete-godot-3d/learn/lecture/41204698#questions
 
-@export var weapon1: Node3D
-@export var weapon2: Node3D
-@export var weapon3: Node3D
-@export var weapon4: Node3D
-
-var index = 0
-const weapon_count = 4
+var index = -1
 
 var current_weapon: Node3D
 
 func _ready() -> void:
-	equip(weapon1)
+	change_weapon()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("switch_weapons"):
@@ -34,7 +28,7 @@ func equip(active_weapon:Node3D) -> void:
 
 # https://www.udemy.com/course/complete-godot-3d/learn/lecture/41204700#questions
 func change_weapon() -> void:
-	index = wrapi(index+1, 0, weapon_count)
+	index = wrapi(index+1, 0, get_child_count())
 	equip(get_child(index))
 
 func shoot(shoot_data:ShootData) -> void:
