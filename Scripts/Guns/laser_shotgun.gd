01737c8ddd8512229dcb3956a84f7a4d10c7174b
nealholt <neal.holts@gmail.com>
2024-09-12 19:50:08 -0600

    LaserShotgun inherits from Gun
    I'm tidying up so much code with this gun inheritance.

--- Scripts/Guns/laser_shotgun.gd
+++ Scripts/Guns/laser_shotgun.gd
@@ -1,59 +1,40 @@
-extends Node3D
+extends Gun
+class_name LaserShotgun
 
 # Here I want to create a "laser" shotgun
 # (by which I mean a hitscan shotgun)
 # like house of the dying sun's blunderbuss
 
+# IMPORTANT NOTE:
+# Bullet bits should be put in place of any other
+# bullet in the "bullet" packed scene.
+
 # Damage is dealt in the bulletbits so that
-# it looks a little more realistic. The bulletbits
-# should never miss and will deal damage when
+# it looks a little more realistic. The
+# bulletbits never miss and will deal damage when
 # they reach the target. There is some travel time
 # even though the hit is determined by raycast
 # in this script.
-
-# Visuals is a Node, not a Node3D because I don't
-# want the bulletbits to inherit position and
-# direction information. If they did, then they
-# would move when the shooter moves and that would
-# be no good.
-
-# +/- this many degrees:
-@export var spread := 5.0
-# number of times to fire per second:
-@export var fire_rate := 5.0
-# number of bullets per shot.
-@export var bullets := 8
-# Range of this weapon
-@export var gun_range:float = 300.0
-
-@export var muzzle_flash:GPUParticles3D # Not currently in use
-# GPU particles to spawn on point of impact:
-@export var sparks:PackedScene # Not currently in use
 
 # Bullet bits are part animation, part actual damage
 # dealer. However, it's predetermined when they get
 # created whether they hit their target or not based
 # on the raycasts in the laser shotgun
-@export var bullet_bit_scene:PackedScene
 
-@onready var cooldown_timer: Timer = $CooldownTimer
+# number of bullets per shot.
+@export var bullets := 8
+# Range of this weapon
+@export var gun_range:float = 300.0
 
-#True if the gun has received command to fire
-var firing: bool = false
-
-# Whether gun is automatic or not. If true then
-# holding the shoot button will fire this weapon.
-# If false, then this weapon will only fire when
-# shoot is first pressed.
-@export var automatic:bool = true
+# GPU particles to spawn on point of impact:
+@export var sparks:PackedScene # Not currently in use
 
 
 func _ready() -> void:
 	# Create all the necessary raycasts
-	var ray:RayCast3D
 	for i in range(bullets):
 		ray = RayCast3D.new()
-		$Raycasts.add_child(ray)
+		$RayCastGroup.add_child(ray)
 		ray.collide_with_areas = true
 		ray.target_position = Vector3(0,0,-gun_range)
 		# Disable collision mask 1
@@ -71,31 +52,28 @@ func _ready() -> void:
 		# issue is. Chalk it up to some aspect of
 		# raycasts I don't understand.
 		ray.rotation_degrees = Vector3(
-				randf_range(-spread,spread),
-				randf_range(-spread,spread),
+				randf_range(-spread_deg,spread_deg),
+				randf_range(-spread_deg,spread_deg),
 				0.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+
+# Override parent class's shoot_actual
+func shoot_actual() -> void:
 	if firing:
 		firing = false
-		if muzzle_flash:
-			muzzle_flash.restart()
-		cooldown_timer.start(1.0/fire_rate)
 		# Loop through all the raycasts
 		# and BulletBits.
-		var ray:RayCast3D
 		var bulletbit:BulletBit
 		for i in range(bullets):
 			# Create a bullet
-			bulletbit = bullet_bit_scene.instantiate()
+			bulletbit = bullet.instantiate()
 			get_tree().get_root().add_child(bulletbit)
 			# Access ith raycast
-			ray = $Raycasts.get_child(i)
+			ray = $RayCastGroup.get_child(i)
 			# Rotate the ray a random amount
 			ray.rotation_degrees = Vector3(
-					randf_range(-spread,spread),
-					randf_range(-spread,spread),
+					randf_range(-spread_deg,spread_deg),
+					randf_range(-spread_deg,spread_deg),
 					0.0)
 			# The following if has not yet been used or tested
 			if ray.is_colliding():
@@ -110,7 +88,3 @@ func _process(_delta: float) -> void:
 				bulletbit.set_up(global_position, ray.get_collision_point(), ray.get_collision_normal(), ray.get_collider())
 			else:
 				bulletbit.set_up(global_position, global_position+ray.global_transform.basis.z * ray.target_position.z, ray.get_collision_normal(), null)
-
-
-func shoot(_shoot_data:ShootData) -> void:
-	firing = cooldown_timer.is_stopped()
