b1e89d79c98534e2abc4081263aa70bcd1bc8c62
nealholt <neal.holts@gmail.com>
2024-09-27 15:09:58 -0600

    Added area bullet

--- Scripts/Projectiles/projectile_area.gd
+++ Scripts/Projectiles/projectile_area.gd
@@ -10,11 +10,24 @@ func set_data(dat:ShootData) -> void:
 	# should not self-whiff
 	$Area3D.set_collision_mask_value(3, dat.use_near_miss)
 
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if (data.collision_exception1 and data.collision_exception1 != area) or (data.collision_exception2 and data.collision_exception2 != area):
+	# If we hit a near-miss detector
+	if area.is_in_group("near-miss detector"):
+		start_near_miss_audio()
+		return
+	elif data.collision_exception1 and data.collision_exception1 == area:
+		return
+	elif data.collision_exception2 and data.collision_exception2 == area:
+		return
+	else:
 		damage_and_die(area)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if (data.collision_exception1 and data.collision_exception1 != body) or (data.collision_exception2 and data.collision_exception2 != body):
+	if data.collision_exception1 and data.collision_exception1 == body:
+		return
+	elif data.collision_exception2 and data.collision_exception2 == body:
+		return
+	else:
 		damage_and_die(body)
