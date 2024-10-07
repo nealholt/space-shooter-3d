12670efd614dca5785af6143a23dcd7fe0c0c193
nealholt <neal.holts@gmail.com>
2024-10-06 20:12:40 -0600

    Proximity fuse bullet in progress

---
+++ Scripts/Projectiles/proximity_fuse_bullet.gd
@@ -0,0 +1,19 @@
+extends ProjectileArea
+
+@export var damaging_explosion:PackedScene
+
+
+# Override
+func damage_and_die(body, collision_point=null):
+	# Add an explosion to main_3d and properly
+	# queue free this ship
+	var explosion = damaging_explosion.instantiate()
+	# Add to main_3d, not root, otherwise the added
+	# node might not be properly cleared when
+	# transitioning to a new scene.
+	Global.main_scene.main_3d.add_child(explosion)
+	# Set explosion's position and damage
+	explosion.global_position = global_position
+	explosion.damage_amt = damage
+	# Wait until the end of the frame to execute queue_free
+	Callable(queue_free).call_deferred()
--- Scripts/explosion_damage_dealing.gd
+++ Scripts/explosion_damage_dealing.gd
@@ -1,11 +1,35 @@
 extends Node3D
+class_name ExplosionDamaging
 
+@export var explosion_vfx:PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var damage_amt:float = 1
 
+# Use this to delete the scene after dealing
+# damage OR after two runs through _process.
+var delete_me:bool = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if !delete_me:
+		# Add an explosion to main_3d and properly
+		# queue free this ship
+		var explosion = explosion_vfx.instantiate()
+		# Add to main_3d, not root, otherwise the added
+		# node might not be properly cleared when
+		# transitioning to a new scene.
+		Global.main_scene.main_3d.add_child(explosion)
+		explosion.global_position = global_position
+	# Damage all overlapping bodies
+	for body in $Area3D.get_overlapping_bodies():
+		if body.is_in_group("damageable"):
+			body.damage(damage_amt)
+			delete_me = true
+	# Damage all overlapping areas
+	for area in $Area3D.get_overlapping_areas():
+		if area.is_in_group("damageable"):
+			area.damage(damage_amt)
+			delete_me = true
+	# Wait until the end of the frame to execute queue_free
+	if delete_me:
+		Callable(queue_free).call_deferred()
+	delete_me = true
