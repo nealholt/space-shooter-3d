a196660684bf47bd7266427f01e010558c01e1c8
nealholt <neal.holts@gmail.com>
2024-10-12 15:31:29 -0600

    Timed fuse bullet fix
    Fixed timed fuse bullet overshoot by using time = dist / (2*speed). Why is the 2 needed? I have no idea, but it worked like a charm.

--- Scripts/Projectiles/timed_fuse_bullet.gd
+++ Scripts/Projectiles/timed_fuse_bullet.gd
@@ -2,9 +2,10 @@ extends ProjectileRay
 
 @export var damaging_explosion:PackedScene
 
-@export var default_timeout:float = 2.0
-# Plus or minus on the explosion timer for variety
-@export var target_range_plus_minus:float = 0.3
+# Add a random amount in the range of
+# plus or minus this percent of bullet timeout
+# on the explosion timer for variety
+@export var target_range_plus_minus:float = 0.1
 
 # Override parent class
 func set_data(dat:ShootData) -> void:
@@ -13,17 +14,33 @@ func set_data(dat:ShootData) -> void:
 	# target and set timer to start with a semi-random
 	# time that will cause an explosion near the target
 	if dat.target:
-		var dist := global_position.distance_to(dat.target.global_position)
-		var timeout := (dist/speed) # + randf_range(-target_range_plus_minus,target_range_plus_minus) #TODO
-		$Timer.stop()
+		# Get distance to target intercept
+		var intercept:Vector3 = Global.get_intercept(
+					global_position, speed,
+					dat.target.global_position,
+					dat.target.velocity)
+		var dist := global_position.distance_to(intercept)
+		# I have no ducking clue why speed needs multiplied
+		# by 2, but I gathered data by manually adjusting
+		# the timeout and distance and got the following
+		# values to hit the player at a bullet speed of 500:
+		# distance, timeout
+		# (305.16226,0.295)
+		# (219.750885,0.215)
+		# (1368.4803,1.369)
+		# If you plot those on a line, the line is almost exactly
+		# y = x/1000
+		# What I'd expect is
+		# y = x/500   because speed was 500 for these tests
+		# Therefore I multiply the speed by 2 and by God
+		# that fixes the overshoot. Still don't know why.
+		var timeout := (dist/(speed*2))
+		# Add in a random +- to the timeout.
+		timeout += randf_range(-timeout*target_range_plus_minus, timeout*target_range_plus_minus)
 		$Timer.start(timeout)
-		#print()
-		#print(timeout) #TODO
-		#print($Timer.wait_time)
-		#print(dist)
-		#print(speed)
 	else:
-		$Timer.start(default_timeout)
+		# If no target, just use default bullet timeout
+		$Timer.start(dat.bullet_timeout)
 
 
 # Explode on timeout
