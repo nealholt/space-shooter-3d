a43223b6635ec135f492412f6b4cbe51d430ca7f
nealholt <neal.holts@gmail.com>
2024-11-02 17:09:57 -0600

    Laser tutorial
    I'm messing with a laser tutorial. Looks pretty good!

---
+++ Scripts/laser.gd
@@ -0,0 +1,23 @@
+extends RayCast3D
+
+# Source:
+# https://www.youtube.com/watch?v=8QumOFwX9Z0
+
+@onready var beam_mesh := $BeamMesh
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var cast_point
+	force_raycast_update()
+	
+	if is_colliding():
+		cast_point = to_local(get_collision_point())
+		
+		beam_mesh.mesh.height = cast_point.y
+		beam_mesh.position.y = cast_point.y/2.0
