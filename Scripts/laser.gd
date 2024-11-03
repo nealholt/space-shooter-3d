e1a719d6729df8d5d8fed0158c00e49e9a128e09
nealholt <neal.holts@gmail.com>
2024-11-03 15:44:13 -0700

    laser modifications

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,12 +1,16 @@
-extends RayCast3D
+extends Node3D
 
 # Source:
 # https://www.youtube.com/watch?v=8QumOFwX9Z0
 # Godot 4: 3D Laser Tutorial
 # by ConnorFoo
+# With modifications and comments by Neal Holtschulte
+@onready var ray := $RayCast3D
 @onready var beam_mesh := $BeamMesh
 @onready var end_particles := $EndParticles
 @onready var beam_particles := $BeamParticles
+
+@export var ray_length:float = 100.0
 
 var tween:Tween
 var beam_radius:float = 0.5
@@ -14,20 +18,21 @@ var beam_radius:float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ray.target_position.y = -ray_length
 	# TODO TESTING. Delete this
 	await get_tree().create_timer(2.0).timeout
-	deactivate(1)
-	await get_tree().create_timer(2.0).timeout
-	activate(1)
+	deactivate(2)
+	await get_tree().create_timer(4.0).timeout
+	activate(2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var cast_point
-	force_raycast_update()
+	var cast_point:Vector3
+	ray.force_raycast_update()
 	
-	if is_colliding():
-		cast_point = to_local(get_collision_point())
+	if ray.is_colliding():
+		cast_point = to_local(ray.get_collision_point())
 		
 		beam_mesh.mesh.height = cast_point.y
 		beam_mesh.position.y = cast_point.y/2.0
@@ -48,31 +53,52 @@ func _process(_delta: float) -> void:
 		)
 
 
+# Time is the duration of the activation animation.
+# Make it large for slow activation, small for quick.
 func activate(time:float) -> void:
-	#tween = Global.main_scene.main_3d.create_tween() #TODO
-	tween = get_tree().create_tween()
+	set_process(true)
+	tween = create_tween()
 	visible = true
 	end_particles.emitting = true
 	beam_particles.emitting = true
+	# set_parallel causes the following tween_property
+	# commands to execute in parallel. In sequence is
+	# the default.
 	tween.set_parallel(true)
 	tween.tween_property(beam_mesh.mesh, "top_radius", beam_radius, time)
 	tween.tween_property(beam_mesh.mesh, "bottom_radius", beam_radius, time)
 	tween.tween_property(beam_particles.process_material, "scale_min", 1.0, time)
 	tween.tween_property(end_particles.process_material, "scale_min", 1.0, time)
+	# "await" pauses the current thread of execution
+	# until a signal is received. Here we await
+	# tween.finished before proceeding.
+	# https://gdscript.com/solutions/coroutines-and-yield/
 	await tween.finished
 
 
+# Time is the duration of the deactivation animation.
+# Make it large for slow deactivation, small for quick.
 func deactivate(time:float) -> void:
-	#tween = Global.main_scene.main_3d.create_tween() #TODO
-	tween = get_tree().create_tween()
+	tween = create_tween()
 	end_particles.emitting = true
 	beam_particles.emitting = true
+	# set_parallel causes the following tween_property
+	# commands to execute in parallel. In sequence is
+	# the default.
 	tween.set_parallel(true)
 	tween.tween_property(beam_mesh.mesh, "top_radius", 0.0, time)
 	tween.tween_property(beam_mesh.mesh, "bottom_radius", 0.0, time)
 	tween.tween_property(beam_particles.process_material, "scale_min", 0.0, time)
 	tween.tween_property(end_particles.process_material, "scale_min", 0.0, time)
+	# "await" pauses the current thread of execution
+	# until a signal is received. Here we await
+	# tween.finished before proceeding.
+	# https://gdscript.com/solutions/coroutines-and-yield/
 	await tween.finished
-	visible = false
 	end_particles.emitting = false
 	beam_particles.emitting = false
+	# Give it another half sec to let the current
+	# particles time out.
+	await get_tree().create_timer(0.5).timeout
+	visible = false
+	set_process(false)
