88181234bfe59634b318db6d072475043af46a11
nealholt <neal.holts@gmail.com>
2025-03-07 14:09:43 -0700

    Revamped main menu

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -135,3 +135,9 @@ func _on_toggle_inverted_toggled(toggled_on: bool) -> void:
 
 func _on_toggle_controls_toggled(toggled_on: bool) -> void:
 	Global.input_man.toggle_mouse_and_keyboard(toggled_on)
+	if toggled_on:
+		$Menu/MouseKeyboardControls.visible = true
+		$Menu/ControllerControls.visible = false
+	else:
+		$Menu/MouseKeyboardControls.visible = false
+		$Menu/ControllerControls.visible = true
