6ecf2ea669494879c82f1defe67dcbedae1cab80
nealholt <neal.holts@gmail.com>
2025-09-21 16:49:45 -0600

    Miscellaneous
    Messing with muzzle flash. Tweaked billboarding of missile smoke.

--- Scripts/vfx/specific_effects/muzzle_flash_cycle.gd
+++ Scripts/vfx/specific_effects/muzzle_flash_cycle.gd
@@ -1,11 +1,24 @@
-extends VisualEffect
+extends Node3D
 
-@onready var sprite := $Sprite3D
+var sprite_list : Array
+var index := 0
 @onready var timer := $Timer
 
-func play() -> void:
-	sprite.visible = true
-	timer.start()
+func _ready() -> void:
+	# Add all spritelist children to the list
+	# and initially make them invisible.
+	for c in get_children():
+		if c is Sprite3D:
+			sprite_list.push_back(c)
+			c.visible = false
+	# Make the first sprite visible
+	sprite_list[index].visible = true
 
-func _on_animation_finished() -> void:
-	sprite.visible = false
+
+func _on_timer_timeout() -> void:
+	# Hide previous sprite
+	sprite_list[index].visible = false
+	# Advance index
+	index = (index + 1) % sprite_list.size()
+	# Show next sprite
+	sprite_list[index].visible = true
