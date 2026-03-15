2c796763e270864d4e88c4eab2651eddba2969bd
nealholt <neal.holts@gmail.com>
2026-03-15 16:13:53 -0600

    New missile lock
    Constant time missile lock option added in.

--- Scripts/MissileLock/missile_lock_timeout.gd
+++ Scripts/MissileLock/missile_lock_timeout.gd
@@ -1,31 +1,39 @@
 extends MissileLock
+# Under this missile lock setup, there's a fixed time to
+# acquire lock no matter what you do. No skill involved
+# besides keeping the target within the maximum angle of
+# screen center.
+# The reticle itself bounces randomly within a steadily
+# decreasing radius of the target reticle.
+# I originally meant for the reticle to slide around within
+# the circle, but this is cool in its own right.
 
+const TIME_TO_LOCK:float = 1.0 ## Time in seconds to acquire a lock
+const RETICLE_DISTANCE:float = 200.0 ## Distance in pixels to start reticle from the target
+
+var lock_time:float ## Time remaining before lock is acquired
+var reticle_radius:float ## Radius of ever-shrinking circle within which reticle moves before lock is acquired
 
 # Takes on-screen position of target as input
 func start_seeking(target_onscreen:Vector2) -> void:
-	# Make reticle approach the target from the far side of
-	# center screen relative to the target's on-screen position.
-	# First, get the position of the target as if 0,0 was center screen
-	reticle_position = (DisplayServer.window_get_size()/2.0) - target_onscreen
-	# Then adjust that position to the far side of center.
-	reticle_position = distance_multiplier*reticle_position + target_onscreen
+	# Start off the reticle at a random angle from the
+	# target_onscreen position, at max distance
+	reticle_radius = RETICLE_DISTANCE # Reset
+	lock_time = TIME_TO_LOCK # Reset
+	var angle:float = randf_range(0,2*PI) # Random angle
+	reticle_position = target_onscreen + Vector2(cos(angle),sin(angle))*reticle_radius
 
 
-# Move reticle into position and, if it's close enough,
-# acquire lock. Move toward target linearly.
+# Move reticle randomly within shrinking reticle_radius.
+# When lock_time reaches zero, acquire lock
 func continue_seeking(delta:float, target_onscreen:Vector2,
 		targeter:Node3D, acquiring:TextureRect) -> void:
-	# Move reticle toward target
-	reticle_position = reticle_position.move_toward(target_onscreen, delta*speed)
-	# "acquiring" is the reticle. Position it on screen
-	acquiring.set_global_position(reticle_position - acquiring_offset)
-	# Check if lock acquired
-	dist_tween_reticles = target_onscreen.distance_squared_to(reticle_position)
-	#print(int(sqrt(dist_tween_reticles)))
-	if dist_tween_reticles < lock_dist_sqd:
+	# Countdown to lock
+	lock_time -= delta
+	if lock_time <= 0.0:
 		lock_acquired.emit(targeter)
-
-
-func stop_seeking() -> void:
-	pass
-	
+	# Reposition reticle
+	reticle_radius = RETICLE_DISTANCE * (lock_time / TIME_TO_LOCK)
+	var angle:float = randf_range(0,2*PI) # Random angle
+	reticle_position = target_onscreen + Vector2(cos(angle),sin(angle))*reticle_radius
+	acquiring.set_global_position(reticle_position - acquiring_offset)
