c0443a93eb3a56a7d9ffe06da6ac488e76a65777
nealholt <neal.holts@gmail.com>
2026-03-23 17:28:30 -0600

    Update explosion_single.gd
    Attempt at debugging difficult-to-replicate bug. A ship explosion kept reloading at the start and end of every level. Weird.

--- Scripts/vfx/specific_effects/explosion_single.gd
+++ Scripts/vfx/specific_effects/explosion_single.gd
@@ -15,6 +15,11 @@ func stop() -> void:
 	super()
 	anim.stop()
 	is_live = false
+	# I think this reset is needed in case the animation
+	# gets interrupted. I'm not sure, but there was a
+	# weird bug where an explosion kept reinitiating
+	# every time I loaded or unloaded a level, so I added this.
+	anim.play("RESET")
 
 func _on_animation_finished() -> void:
 	super()
