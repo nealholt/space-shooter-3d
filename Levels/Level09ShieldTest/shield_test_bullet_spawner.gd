d73aafa8b845e769763e2e572682a6711c9083f2
nealholt <neal.holts@gmail.com>
2026-04-21 20:21:59 -0600

    Deleted projectiles
    Got rid of a lot of scenes that are no longer needed because the bullet spawner composes the pieces on the fly.

---
+++ Levels/Level09ShieldTest/shield_test_bullet_spawner.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+# Set up the scene
+func _ready() -> void:
+	var b:Projectile = BulletSpawner.new_bullet(BulletSpawner.BULLET_TYPE.BASIC_RAY)
+	b.speed = 5.0
+	b.time_out = 40.0
+	b.position = Vector3(0.326, 0.0, 10.85)
+	add_child(b)
+	
+	b = BulletSpawner.new_bullet(BulletSpawner.BULLET_TYPE.BASIC_RAY)
+	b.speed = 5.0
+	b.time_out = 40.0
+	b.position = Vector3(0.0, 0.0, -37.0)
+	b.rotation_degrees = Vector3(0.0, -180.0, 0.0)
+	add_child(b)
+	
+	b = BulletSpawner.new_bullet(BulletSpawner.BULLET_TYPE.BASIC_RAY)
+	b.speed = 5.0
+	b.time_out = 40.0
+	b.position = Vector3(0.0, -13.78, 0.0)
+	b.rotation_degrees = Vector3(90.0, 0.0, 0.0)
+	add_child(b)
+	
+	b = BulletSpawner.new_bullet(BulletSpawner.BULLET_TYPE.BASIC_RAY)
+	b.speed = 5.0
+	b.time_out = 40.0
+	b.position = Vector3(18.11, 0.0, 0.0)
+	b.rotation_degrees = Vector3(0.0, 90.0, 0.0)
+	add_child(b)
