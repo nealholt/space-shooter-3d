f56ecde5fa3d934d0e2892c250e2f7910f4ff8bb
nealholt <neal.holts@gmail.com>
2026-07-22 15:51:09 -0600

    Commenting flee state

--- Controllers/States/state_flee.gd
+++ Controllers/States/state_flee.gd
@@ -1,10 +1,14 @@
 class_name StateFlee extends State
 
-# This is basically going to be the inverse of seek
+# This is basically going to be the inverse of Attack
 # Roll/yaw/pitch away from target and accelerate to
 # max until timeout or max_distance reached.
-# If max_distance reached, transition to seek.
-# If timeout reached, transition to a random evasion.
+# When max_distance or timeout is reached, transition to Attack.
+
+# The new addition is detecting if the target is
+# facing us, if it is, pitch to put the target above us.
+# The goal is to prevent ships from fleeing in a
+# straight line when their attacker is right behind them.
 
 # This value is over-written by the npc_controller
 var distance_limit_sqd:float = 0.0 # meters
@@ -38,17 +42,17 @@ func Physics_Update(delta:float, motion:MovementProfile, orientation_data:Target
 	# above. (So we are moving across enemy's line of sight.)
 	# Specifically if target is more than 75% facing us.
 	elif orientation_data.amt_target_facing_us > 0.75:
-		# TODO TESTING TODO LEFT OFF HERE
 		#print('pitching to get target above')
 		# Slow slightly for better pitching
 		motion.goal_speed = 0.9
 		motion.pitch_target_above(orientation_data, obstacle_detector)
 	
-	# Otherwise pitch away from target
+	# Otherwise pitch to put target behind us at top speed
 	else:
 		motion.goal_speed = 1.0 # Top speed
 		#pitch_target_behind()
 		motion.pitch_target_behind_serious(orientation_data) # ignore obstacle detector
+		# pitch_target_behind_serious ignores the obstacle detector.
 		# The reason we need to ignore the obstacle detector is
 		# because if we are too close to a target, the obstacle
 		# detector can suppress pitching in BOTH directions
