db410514221edf7351bc20ae32bf77c073ff03fd
nealholt <neal.holts@gmail.com>
2026-07-26 17:05:04 -0600

    Drawing AABB
    Fixed a visual bug and learned about GPUParticles' AABB boxes... they might need to be LARGE in a space game!

--- vfx/DamageEffects/burning_trail.gd
+++ vfx/DamageEffects/burning_trail.gd
@@ -1,5 +1,11 @@
 class_name BurningTrail extends Node3D
 
+# NOTE: Particles disappear and then sometimes annoyingly reappear
+# when the camera moves away and then moves back to them. The issue
+# can be addressed under Drawing -> Visibility AABB
+# See also: https://docs.godotengine.org/en/stable/classes/class_aabb.html#class-aabb
+# And: https://forum.godotengine.org/t/regarding-off-camera-particle-emision/27366
+
 @onready var major_damage: Node3D = $MajorDamage
 @onready var major_damage_line_sparks: Node3D = $MajorDamageLineSparks
 @onready var mild_damage: Node3D = $MildDamage
@@ -11,7 +17,7 @@ class_name BurningTrail extends Node3D
 # ship.gd connects health_lost signal to this function
 func display_damage(health:HealthComponent, _amount:float) -> void:
 	var percent_health:float = health.get_percent_health()
-	if percent_health <= 0.0: #This will happen for death animation
+	if percent_health <= 0.1: # This will happen for death animation too
 		mild_damage.stop_emitting()
 		major_damage_line_sparks.stop_emitting()
 		major_damage.start_emitting()
--- vfx/DamageEffects/emission_controller.gd
+++ vfx/DamageEffects/emission_controller.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+# NOTE: Particles disappear and then sometimes annoyingly reappear
+# when the camera moves away and then moves back to them. The issue
+# can be addressed under Drawing -> Visibility AABB
+# See also: https://docs.godotengine.org/en/stable/classes/class_aabb.html#class-aabb
+# And: https://forum.godotengine.org/t/regarding-off-camera-particle-emision/27366
+
 func start_emitting() -> void:
 	for n in get_children():
 		n.set_emitting(true)
